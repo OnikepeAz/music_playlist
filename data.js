57e323c179f24aa5fa5bd28da1bd8103425d6799
OnikepeAz <azeezonikepe@gmail.com>
2022-12-25 13:41:59 +0100

    changes

---
+++ data.js
@@ -0,0 +1,26 @@
+export const playlist = [
+  {
+    id: 1,
+    name: "D4L",
+    artist: "Future, Drake & Young Thug",
+    genre: "HipHop",
+    albumCoverUrl: "https://i.ibb.co/9rTBzmD/D4L.jpg",
+    youtubeID: "ldeLy8D_USE",
+  },
+  {
+    id: 2,
+    name: "Why would I Stop?",
+    artist: "Big Sean",
+    genre: "HipHop/Rap",
+    albumCoverUrl: "https://i.ibb.co/8g4PFQp/Big-Sean.png",
+    youtubeID: "httEG6zsM_U",
+  },
+  {
+    id: 3,
+    name: "Love Yourz",
+    artist: "JCole",
+    genre: "Rap",
+    albumCoverUrl: "https://i.ibb.co/BZSJPVV/Jcole.jpg",
+    youtubeID: "Ka4BxFizU7I",
+  }
+]
